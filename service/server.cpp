c60c89fdb60401cb14178d8fe7708d5f29d13377
Ilya <ilyegorow@yandex.ru>
2025-06-24 16:46:56 +0300

    upd structure project, add .clang-format

---
+++ service/server.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <filesystem>
+#include <fsream>
+#include <sdbus-c++/sdbus-c++.h>
+#include <nlohmann/json.hpp>
+#include <map>
+#include <string>
+
+static const char* configDir = ".com.system.configurationManager";
+
+class ApplicationObject
+{
+
+}
+
+int main()
+{
+    std::cout << "Nya";
+}
